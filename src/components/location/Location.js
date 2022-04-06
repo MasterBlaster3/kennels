df6505825eeb3ea808e06f99d68b0c797f3c18cc
MasterBlaster3 <josephmaifeld@gmail.com>
2022-04-06 09:35:20 -0500

    initial commit

---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="locations">
+        <h3 className="location">Nashville Kennels North</h3>
+        <div className="location__address">200 Main Street</div>
+    </section>
+)
